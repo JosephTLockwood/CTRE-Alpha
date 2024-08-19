8b3861649860ac3e1912bb6737e2322fc4f86d7d
JosephTLockwood <joeojazz@gmail.com>
2024-08-19 18:38:54 -0400

    New git

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "CTRE-Alpha";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "0bc3f55e32e1a70ba0e6fab1658b7d2b71d47071";
-  public static final String GIT_DATE = "2024-08-19 18:33:29 EDT";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "4ffcf0f85191817da09164d401ccbdcb2f70afc2";
+  public static final String GIT_DATE = "2024-08-19 18:37:01 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-08-19 18:34:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1724106878055L;
+  public static final String BUILD_DATE = "2024-08-19 18:38:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1724107085700L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
