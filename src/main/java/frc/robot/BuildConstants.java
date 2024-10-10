5655063652eacc7799059039a744e8516ab4e75f
JosephTLockwood <joeojazz@gmail.com>
2024-10-10 00:07:37 -0400

    Scrap that

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "CTRE-Alpha";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 70;
-  public static final String GIT_SHA = "5977f4a764ef3b90e8589d2e693c94eb22190a70";
-  public static final String GIT_DATE = "2024-10-09 23:52:09 EDT";
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "4300464b98da24db53753e88b8aae35419410663";
+  public static final String GIT_DATE = "2024-10-10 00:00:44 EDT";
   public static final String GIT_BRANCH = "Hoot-Replay";
-  public static final String BUILD_DATE = "2024-10-10 00:00:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1728532814323L;
+  public static final String BUILD_DATE = "2024-10-10 00:07:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1728533251621L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
